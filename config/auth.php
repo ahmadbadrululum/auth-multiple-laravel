cedb2c6f24ea179c9514d38be63a5a6cb19c0459
Ahmad Badrul Ulum <ahmadbadrul635@gmail.com>
2019-05-23 03:12:59 +0700

    error nggabisa masuk ke table admin

--- config/auth.php
+++ config/auth.php
@@ -85,7 +85,7 @@ return [
 
         'admins' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'model' => App\Models\Admin::class,
         ],
 
         // 'users' => [
